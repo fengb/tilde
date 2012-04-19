29ae68665f55d2d03a469ee11dea2f2298eb1428
fengb <benjamin.feng@gmail.com>
2012-04-19 14:55:08 -0500

    Added Rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tilde::Application.config.secret_token = '6f1f14a40dda56114e54ebaf1343301faa47c75a253359ead0f0a7e36e271e2905e273bfd6561e187ee0a9a949846314f81863aea77259b11a3373e4472fee6a'
