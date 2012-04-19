fc40d4ac84e83db7def9f7fb1ce721e053928905
fengb <benjamin.feng@gmail.com>
2012-04-19 17:16:25 -0500

    Added fork scaffolding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def command
+    # TODO: implement me
+    spawned = false
+    port = 3001
+
+    if !spawned
+      spawn(port)
+    end
+    response = communicate(port)
+    render :text => response
+  end
+
+  private
+  def spawn(port)
+    fork do
+      loop do
+        repl
+      end
+    end
+  end
+
+  def communicate(port)
+    # TODO: implement me
+    "Hi"
+  end
+
+  def repl
+    # TODO: implement me
+    $stderr.puts "I'm alive!"
+    sleep 10
+  end
 end
