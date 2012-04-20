cffd834b5b6bfee8cd76d10a88ab1e04ab4b823e
Pete Brousalis <pbrousalis@enova.com>
2012-04-20 12:54:15 -0500

    Added hilarious Quake styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,9 @@ $(function () {
           success: function(e) {
             jqconsole.Write(e.response + '\n', 'jqconsole-output');
             startPrompt();
+          },
+          failure: function(e) {
+            jqconsole.Write('There was an error with your request' + '\n', 'jqconsole-output');
           }
         }); 
       }
@@ -35,8 +38,9 @@ $(function () {
     if (e.keyCode == 192) { 
       open = !open
       $('#console').animate({
-        top: open ? "0" : "-500"
+        top: open ? "0" : "-505"
       });
+      jqconsole.Focus();
       return false;
     }
   });
